7fbeedc3e82abbd09f1732da24e1c454f0ebb7e4
only-zover <j0seextremehd@outlook.com>
2022-10-31 22:53:06 -0300

    Melhoria de código

--- scripts/login.js
+++ scripts/login.js
@@ -1,27 +1,68 @@
 const bt = document.querySelector('#bt')
+const msg = document.querySelector('#msg')
+
 const user = document.querySelector('#user')
+const labelUser = document.querySelector('#labelUser')
+let validUser = false
+
 const password = document.querySelector('#password')
+const labelPassword = document.querySelector('#labelPassword')
+let validPassword = false
 
-bt.addEventListener('click', () => {
-    let list = {
-        "email": user.value,
-        "password": password.value
-    }
+user.addEventListener('keyup', () => {checkUser()})
+password.addEventListener('keyup', () => {checkPassword()})
+bt.addEventListener('click', () => {login()})
 
-    list = JSON.stringify(list)
-
-    var xhr = new XMLHttpRequest()
+function login() {
+    if (validUser && validPassword) {
+        let userInfo = {
+            "email": user.value,
+            "password": password.value
+        }
+    
+        userInfo = JSON.stringify(userInfo)
+    
+        var xhr = new XMLHttpRequest()
+    
         xhr.open("POST", "https://reqres.in/api/login", true)
         xhr.setRequestHeader("Content-type", "application/json")
-        xhr.send(list)
-        xhr.onreadystatechange = function() {
-  
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            let data = xhr.responseText;
-            let token = data.split('"')[3]
-
-            localStorage.setItem('token', token)
-            window.location.href = 'search.html'
-            }
+        xhr.send(userInfo)
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                let data = xhr.responseText;
+                let token = data.split('"')[3]
+    
+                localStorage.setItem('token', token)
+                window.location.href = 'search.html'
+            } else if (xhr.readyState == 4 && xhr.status == 400) {
+                msg.innerHTML = 'Email ou senha inválida'
+            } 
         }
-})
+    } else {
+        msg.innerHTML = 'A senha e/ou email não podem ter menos de 3 caracteres'
+    }
+}
+
+function checkUser() {
+    if (user.value.length < 3) {
+        labelUser.setAttribute('style', 'color: red')
+        labelUser.innerHTML = 'Email: (No mínimo 3 caracteres)'
+        validUser = false
+    } else {
+        labelUser.setAttribute('style', 'color: green')
+        labelUser.innerHTML = 'Email:'
+        validUser = true
+    }
+}
+
+function checkPassword() {
+    if (password.value.length < 3) {
+        labelPassword.setAttribute('style', 'color: red')
+        labelPassword.innerHTML = 'Senha: (No mínimo 3 caracteres)'
+        validPassword = false
+    } else {
+        labelPassword.setAttribute('style', 'color: green')
+        labelPassword.innerHTML = 'Senha:'
+        validPassword = true
+    }
+}
